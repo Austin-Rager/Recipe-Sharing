2658128be6e6687d63cb82a576e09f837b0d0eee
Austin-Rager <austinrager01@gmail.com>
2025-07-15 10:55:57 -0600

    pushing backend

--- backend/backend.js
+++ backend/backend.js
@@ -24,7 +24,7 @@ const {Account, Recipe} = require("./model.js");
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
